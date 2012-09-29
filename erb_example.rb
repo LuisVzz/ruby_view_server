a6ce5af851c95bbafb30ce22f584d4411808a8ed
Luis Roberto Vazquez <stk.walshy@gmail.com>
2012-09-28 22:15:47 -0500

    Inserting some strings, hashes, arrays and integers.

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,28 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+y = 2
+animals = ["cat","aligator","whale"]
+me = {:name => "Roberto", :eyes => "brown", :shirt =>"grey"} 
+numbers = [1, 2, 3, 4]
+
+erb_string = "
+
+<h1><%= me[:name] + '\\'s ' + 'Blog'%></h1>
+
+<ul>
+	<% animals.each do |animal| %>
+		<li><%= animal.upcase.reverse %></li>
+	<% end %>
+</ul>
+ 
+<p><%= 'My name is ' + me[:name] + ', my eyes are ' + me[:eyes] + '.' %></p>
+
+<p><%= 'Let\\'s do some numbers! '  + numbers.last.to_s + '! is ' + numbers.inject(:*).to_s %></p>
+"
+
+
+
+template = ERB.new(erb_string)
 puts template.result(binding)
+
